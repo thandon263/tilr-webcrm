0487e6584982266e4e1a7c3d6f3ea8bb2e2ba7da
thandon263 <ncubethando20@gmail.com>
2016-11-15 03:29:18 -0500

    Adding contacts from the random user api

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,19 +1,33 @@
 // AJAX request to the Random User Generator API.
 $(function () {
 
-    $.ajax({
-      url: 'https://randomuser.me/api/?results=10',
-      method: 'GET',
-      data: {},
-      dataType: 'json'
-    }).done(function(responseData){
-      console.log(responseData);
-      $('tbody').html("<tr><td>" + responseData.results[0].name["first"] + "</td><td>" + responseData.results[0].name["last"] + "</td><td>" + responseData.results[0].location["street"] + "</td><td>" + responseData.results[0].dob.split('')[0] + "</td>" + responseData.results[0].location["street"] + "</tr>");
-    }).success(function(){
-      console.log("Request Complete!!");
-    }).always(function(){
-      console.log("JSON object is the best response data in Javascript. Easy to manipulate.");
-    }); // End of AJAX method
+  var refresh = $.ajax({
+                    url: 'https://randomuser.me/api/?page=3&results=10&seed=abc',
+                    method: 'GET',
+                    data: {},
+                    dataType: 'json'
+                  }).done(function(responseData){
+                    // Variable to stored data after looping through responseData
+                    var contact_details = '';
+                    // log results on the console
+                    console.log(responseData);
+                    // Loop through responseData and push into contact_details
+                    $.each(responseData.results, function(i, item) {
 
+                      contact_details += "<tr><td>" + responseData.results[i].name["first"] + "</td><td>" + responseData.results[i].name["last"] + "</td><td>" + responseData.results[i].location["street"] + "</td><td>" + responseData.results[i].gender + "</td></tr>";
+
+                    });
+
+                    $('tbody').append(contact_details);
+
+                  }).success(function(){
+                    console.log("Request Complete!!");
+                  }).always(function(){
+                    console.log("JSON object is the best response data in Javascript. Easy to manipulate.");
+                  }).error(function() {
+
+                    console.alert("Request Failed!!")
+
+                }); // End of AJAX method
 
 });// End of document ready function
