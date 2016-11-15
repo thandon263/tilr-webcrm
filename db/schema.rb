21799d6642f9622cbeacefcbc0f60367eff54eaa
thandon263 <ncubethando20@gmail.com>
2016-11-14 20:12:23 -0500

    Adding contacts seeds to test application and mark up for contacts show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114231445) do
+ActiveRecord::Schema.define(version: 20161114235630) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "address"
+    t.string   "age"
+    t.string   "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
