4789f692a2636e91385b6033a376e6f1a2881b3e
thandon263 <ncubethando20@gmail.com>
2016-11-20 12:03:32 -0500

    Database setup

--- Gemfile
+++ Gemfile
@@ -4,7 +4,11 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', :group => [:development, :test]
+group :production do
+  gem 'thin'
+  gem 'pg'
+end
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -27,7 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'capistrano-rails', group: :production
 
 # Interface between Ruby and ImageMagick
 gem 'rmagick', '~> 2.15', '>= 2.15.4'
--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,19 @@
 
 ActiveRecord::Schema.define(version: 20161116164329) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.text     "address"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
     t.string   "gender"
     t.string   "state"
-    t.         "pictures"
-    t.         "picture"
-    t.         "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.json     "picture"
+    t.json     "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,10 +40,9 @@ ActiveRecord::Schema.define(version: 20161116164329) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.         "pictures"
-    t.         "picture"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.json     "picture"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
