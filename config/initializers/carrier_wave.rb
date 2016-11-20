c5e403e000d8795cfe74e3bbe6f8853631970054
thandon263 <ncubethando20@gmail.com>
2016-11-20 12:08:32 -0500

    Database setup

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV["aws_access_id"],
-    aws_secret_access_key: ENV["aws_access_secret_key"],
+    aws_access_id:     ENV["aws_access_id"],
+    aws_access_secret_key: ENV["aws_access_secret_key"],
   }
   config.fog_directory  = "mypictures"
 end
